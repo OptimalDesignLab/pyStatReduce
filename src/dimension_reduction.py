7759f063bb75a696c2be9f47fa3946b181fccd5f
Kinshuk Panda <pandak@rpi.edu>
2018-01-10 11:35:18 -0500

    added a test of dimension reduction straetegy

--- src/dimension_reduction.py
+++ src/dimension_reduction.py
@@ -8,14 +8,14 @@ class DimensionReduction(object):
     def __init__(self, threshold_factor):
         self.threshold_factor = threshold_factor
 
-    def getDominantDistribution(self, QoI, jdist):
+    def getDominantDirections(self, QoI, jdist):
 
         mu = cp.E(jdist)
         covariance = cp.Cov(jdist)
 
         # Check if variance covariance matrix is diagonal
         if np.count_nonzero(covariance - np.diag(np.diagonal(covariance))) == 0:
-            sqrt_Sigma = np.sqrt(Sigma)
+            sqrt_Sigma = np.sqrt(covariance)
         else:
             raise NotImplementedError
 
@@ -24,7 +24,7 @@ class DimensionReduction(object):
         Hessian = QoI.eval_QoIHessian(mu, xi)
 
         # Compute the eigen modes of the Hessian of the Hadamard Quadratic
-        Hessian_Product = np.matmul(sqrt_Sima, np.matmul(H, sqrt_Sigma))
+        Hessian_Product = np.matmul(sqrt_Sigma, np.matmul(Hessian, sqrt_Sigma))
         eigen_vals, eigen_vectors = np.linalg.eig(Hessian_Product)
 
         # Get the system energy of Hessian_Product
@@ -32,19 +32,18 @@ class DimensionReduction(object):
         ind = []
 
         # get the indices of dominant eigenvalues in descending order
-        sort_ind = np.argsort(eigen_vals)
+        sort_ind = eigen_vals.argsort()[::-1] # np.argsort(eigen_vals)
 
         # Check the threshold
         for i in xrange(0, QoI.systemsize):
-            dominant_eigen_val_ind = sort_ind[0:i]
+            dominant_eigen_val_ind = sort_ind[0:i+1]
             reduced_energy = np.sum(eigen_vals[dominant_eigen_val_ind])
             if reduced_energy <= self.threshold_factor*system_energy:
-                # ind = np.append(ind, dominant_eigen_val_ind[i])
                 ind.append(dominant_eigen_val_ind[i])
             else:
                 break
 
         if len(ind) == 0:
-            ind.append(max(eigen_vals))
+            ind.append(np.argmax(eigen_vals))
 
         return eigen_vals, eigen_vectors, ind
