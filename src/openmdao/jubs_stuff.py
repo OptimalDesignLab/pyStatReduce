b850f854272feee39ecacf9a985cf413ce495984
Kinshuk Panda <pandak@rpi.edu>
2018-08-15 17:16:20 -0400

    addded a recursive functions for location and weights

--- src/openmdao/jubs_stuff.py
+++ src/openmdao/jubs_stuff.py
@@ -35,18 +35,31 @@ class PerturbationComp(ExplicitComponent):
 
     def setup(self):
         print("Within PerturbationComp Setup")
+
+        # Inputs
         self.covariance = self.options['distribution_matrix']
         self.add_input('mu', shape=self.options['n_random'])
-        self.xi = np.zeros([N_RANDOM, N_SAMPLES])
-        self.compute_perturbation()
-        self.add_output('xi', shape=(N_RANDOM,N_SAMPLES))
+        self.xi = np.zeros([N_SAMPLES, N_RANDOM])
+        idx = 0
+        ctr = 0
+        colloc_xi_arr = np.zeros(self.options['n_random'])
+        ref_collocation_pts, _ = np.polynomial.hermite.hermgauss(self.options['degree'])
+        sqrt_Sigma = np.sqrt(self.covariance)
+        idx, ctr = self.compute_perturbation(sqrt_Sigma, ref_collocation_pts, colloc_xi_arr, self.xi, idx, ctr)
+        assert idx == -1
+
+        # Outputs
+        self.add_output('xi', shape=(N_SAMPLES, N_RANDOM))
 
     def compute(self, inputs, outputs):
 
-        outputs['xi'] = (inputs['mu'] + self.xi.T).T
+        # outputs['xi'] = (inputs['mu'] + self.xi.T).T
+        outputs['xi'] = inputs['mu'] + self.xi
         # print(inputs['mu'] + self.xi)
 
-    def compute_perturbation(self):
+    def compute_perturbation(self, sqrt_Sigma, ref_collocation_pts, colloc_xi_arr,
+                             actual_location, idx, ctr):
+        """
         # TODO: generalize this to multiple dimensions
         sqrt2 = np.sqrt(2)
         sqrt_Sigma = np.sqrt(self.covariance)
@@ -55,6 +68,25 @@ class PerturbationComp(ExplicitComponent):
                 idx = DEGREE*i + j
                 self.xi[:,idx] = sqrt2* \
                                  np.matmul(sqrt_Sigma, np.array([SCLOC_Q[i], SCLOC_Q[j]]))
+        """
+        if idx == colloc_xi_arr.size-1:
+            sqrt2 = np.sqrt(2)
+            for i in xrange(0, ref_collocation_pts.size):
+                # colloc_w_arr[idx] = ref_collocation_w[i] # Get the array of all the weights needed
+                colloc_xi_arr[idx] = ref_collocation_pts[i] # Get the array of all the locations needed
+                actual_location[ctr,:] = sqrt2*np.matmul(sqrt_Sigma, colloc_xi_arr)
+                ctr += 1
+            return idx-1, ctr
+        else:
+            for i in xrange(0, ref_collocation_pts.size):
+                colloc_xi_arr[idx] = ref_collocation_pts[i]
+                # colloc_w_arr[idx] = ref_collocation_w[i]
+                idx, ctr = self.compute_perturbation(sqrt_Sigma, ref_collocation_pts,
+                                        colloc_xi_arr,
+                                        actual_location,
+                                        idx+1, ctr)
+            return idx-1, ctr
+
 
 
 class Rosenbrock_OMDAO(ExplicitComponent):
@@ -98,9 +130,17 @@ class StochasticCollocation(ExplicitComponent):
         for i in xrange(0, degree**systemsize):
             self.add_input('fval{}'.format(i), val=0.0)
 
-        self.add_output('mu_j', val=0.0)
+        ref_collocation_w = self.options['weights']
+        idx = 0
+        ctr = 0
+        colloc_w_arr = np.zeros(systemsize)
         self.quadrature_weights = np.zeros(degree**systemsize)
-        self.compute_weights()
+        self.compute_weights(ref_collocation_w, colloc_w_arr,
+                             self.quadrature_weights, idx, ctr)
+
+
+        # Outputs
+        self.add_output('mu_j', val=0.0)
 
     def compute(self, inputs, outputs):
         systemsize = self.options['n_random']
@@ -114,13 +154,28 @@ class StochasticCollocation(ExplicitComponent):
         outputs['mu_j'] = mu_j
         print('mu_j = ', mu_j)
 
-    def compute_weights(self):
+    def compute_weights(self, ref_collocation_w, colloc_w_arr, quadrature_weights, idx, ctr):
+        """
         ref_collocation_w = self.options['weights']
         for i in xrange(0, DEGREE):
             for j in xrange(0, DEGREE):
                 idx = DEGREE*i + j
                 self.quadrature_weights[idx] = ref_collocation_w[i] \
                                                * ref_collocation_w[j]
+        """
+        if idx == colloc_w_arr.size-1:
+            for i in xrange(0, ref_collocation_w.size):
+                colloc_w_arr[idx] = ref_collocation_w[i] # Get the array of all the weights needed
+                quadrature_weights[ctr] = np.prod(colloc_w_arr)
+                ctr += 1
+            return idx-1, ctr
+        else:
+            for i in xrange(0, ref_collocation_w.size):
+                colloc_w_arr[idx] = ref_collocation_w[i]
+                idx, ctr = self.compute_weights(ref_collocation_w, colloc_w_arr,
+                                                quadrature_weights,
+                                                idx+1, ctr)
+            return idx-1, ctr
 
 
 p.model.add_subsystem('perturb', PerturbationComp(degree=DEGREE,
@@ -135,7 +190,7 @@ p.model.add_subsystem('multi_point', Group())
 for i in range(N_SAMPLES):
     name = 'R{}'.format(i)
     p.model.multi_point.add_subsystem(name, Rosenbrock_OMDAO(size=N_RANDOM))
-    p.model.connect('perturb.xi', 'multi_point.{}.rv'.format(name), src_indices=[(0,i), (1,i)])
+    p.model.connect('perturb.xi', 'multi_point.{}.rv'.format(name), src_indices=[(i,0), (i,1)])
     # p.model.connect('{}.fval'.format(name), 'stochastic_cloc.fval{}'.format(i))
 
 p.model.add_subsystem('stochastic_colloc', StochasticCollocation(degree=DEGREE,
