cc5d29a3657f5db30ef7d6cf5e040ae0f3fcd4e9
Kinshuk Panda <pandak@rpi.edu>
2018-08-15 15:02:58 -0400

    The openmdao style implementation of the Rosenbrock function works, and progress on the recursive function

--- src/openmdao/jubs_stuff.py
+++ src/openmdao/jubs_stuff.py
@@ -34,16 +34,19 @@ class PerturbationComp(ExplicitComponent):
         self.options.declare('locations', types=np.ndarray)
 
     def setup(self):
+        print("Within PerturbationComp Setup")
         self.covariance = self.options['distribution_matrix']
-        self.add_input('mu', shape=N_RANDOM)
+        self.add_input('mu', shape=self.options['n_random'])
         self.xi = np.zeros([N_RANDOM, N_SAMPLES])
         self.compute_perturbation()
         self.add_output('xi', shape=(N_RANDOM,N_SAMPLES))
 
     def compute(self, inputs, outputs):
-        inputs['xi'] = inputs['mu'] + self.xi
 
-    def compute_perturbation():
+        outputs['xi'] = (inputs['mu'] + self.xi.T).T
+        # print(inputs['mu'] + self.xi)
+
+    def compute_perturbation(self):
         # TODO: generalize this to multiple dimensions
         sqrt2 = np.sqrt(2)
         sqrt_Sigma = np.sqrt(self.covariance)
@@ -63,6 +66,7 @@ class Rosenbrock_OMDAO(ExplicitComponent):
         self.options.declare('size', types=int)
 
     def setup(self):
+        print('Within Rosenbrock_OMDAO setup')
         self.add_input('rv', val = np.zeros(N_RANDOM)) # rv = random variable
         self.add_output('fval', val=0.0)
 
@@ -71,7 +75,7 @@ class Rosenbrock_OMDAO(ExplicitComponent):
         outputs['fval'] = sum(100.0*(rv[1:]-rv[:-1]**2.0)**2.0 + (1-rv[:-1])**2.0)
 
 
-class StochasticCollocation(ExplicitComponent)
+class StochasticCollocation(ExplicitComponent):
     """
     Class that aggregates the function values at the random variable quadrature
     locations.
@@ -82,16 +86,41 @@ class StochasticCollocation(ExplicitComponent)
         self.options.declare('weights', types=np.ndarray)
 
     def setup(self):
-        self.add_input('fval', val=0.0)
+        print('Within StochasticCollocation setup')
+        systemsize = self.options['n_random']
+        degree = self.options['degree']
+
+        # self.add_input('fval', val=np.zeros(degree**systemsize))
+
+        # Since OpenMDAO does not have anything like target_indices, you need to
+        # loop over all the components from which you need to get the output
+        # function values.
+        for i in xrange(0, degree**systemsize):
+            self.add_input('fval{}'.format(i), val=0.0)
+
         self.add_output('mu_j', val=0.0)
+        self.quadrature_weights = np.zeros(degree**systemsize)
+        self.compute_weights()
 
     def compute(self, inputs, outputs):
-        # Boo!
+        systemsize = self.options['n_random']
+        degree = self.options['degree']
+        J_determ = np.zeros(degree**systemsize)
 
-    def compute_weights
+        for i in xrange(0, degree**systemsize):
+            J_determ[i] = inputs['fval{}'.format(i)]
 
+        mu_j = np.sum(J_determ * self.quadrature_weights) / (np.sqrt(np.pi)**systemsize)
+        outputs['mu_j'] = mu_j
+        print('mu_j = ', mu_j)
 
-
+    def compute_weights(self):
+        ref_collocation_w = self.options['weights']
+        for i in xrange(0, DEGREE):
+            for j in xrange(0, DEGREE):
+                idx = DEGREE*i + j
+                self.quadrature_weights[idx] = ref_collocation_w[i] \
+                                               * ref_collocation_w[j]
 
 
 p.model.add_subsystem('perturb', PerturbationComp(degree=DEGREE,
@@ -106,10 +135,18 @@ p.model.add_subsystem('multi_point', Group())
 for i in range(N_SAMPLES):
     name = 'R{}'.format(i)
     p.model.multi_point.add_subsystem(name, Rosenbrock_OMDAO(size=N_RANDOM))
-    p.model.connect('perturb.xi', '{}.rv'.format(name), src_indices=[i])
-    p.model.connect('{}.fval'.format(name), 'stochastic_cloc.fval{}'.format(i))
+    p.model.connect('perturb.xi', 'multi_point.{}.rv'.format(name), src_indices=[(0,i), (1,i)])
+    # p.model.connect('{}.fval'.format(name), 'stochastic_cloc.fval{}'.format(i))
 
-p.model.add_subsystem('stochastic_cloc', StochasticCollocation(degree=DEGREE,
+p.model.add_subsystem('stochastic_colloc', StochasticCollocation(degree=DEGREE,
                                          n_random=N_RANDOM,
                                          weights=SCLOC_W),
-                    promotes_outputs=['mu_j'])
+                                         promotes_outputs=['mu_j'])
+
+for i in range(N_SAMPLES):
+    name = 'R{}'.format(i)
+    p.model.connect('multi_point.{}.fval'.format(name), 'stochastic_colloc.fval{}'.format(i))
+    # p.model.connect('stochastic_colloc.fval', 'multi_point.{}.fval'.format(name), src_indices=[i])
+
+p.setup()
+p.run_model()
--- src/test_python_concepts.py
+++ src/test_python_concepts.py
@@ -5,41 +5,48 @@
 
 import numpy as np
 import chaospy as cp
-import examples
-from stochastic_collocation import StochasticCollocation
-from quantity_of_interest import QuantityOfInterest
-from dimension_reduction import DimensionReduction
-from stochastic_arnoldi.arnoldi_sample import ArnoldiSampling
 
-# 1. Check the quadrature
+def recursive_function(sigma, ref_collocation_pts, ref_collocation_w,colloc_xi_arr,
+                        colloc_w_arr, actual_location, idx, ctr, index_list=[0,]):
+
+    if idx == colloc_xi_arr.size-1:
+        print "index_list = ", index_list
+        sqrt2 = np.sqrt(2)
+        for i in xrange(0, ref_collocation_pts.size):
+            colloc_w_arr[idx] = ref_collocation_w[i] # Get the array of all the weights needed
+            colloc_xi_arr[idx] = ref_collocation_pts[i] # Get the array of all the locations needed
+            actual_location[ctr,:] = sqrt2*sigma*colloc_xi_arr
+            print "ctr = ", ctr
+            ctr += 1
+        index_list.pop(-1)
+        return idx-1, ctr
+    else:
+        print "index_list = ", index_list
+        for i in xrange(0, ref_collocation_pts.size):
+            colloc_xi_arr[idx] = ref_collocation_pts[i]
+            colloc_w_arr[idx] = ref_collocation_w[i]
+            index_list.append(idx+1)
+            idx, ctr = recursive_function(sigma, ref_collocation_pts,
+                                    ref_collocation_w,
+                                    colloc_xi_arr, colloc_w_arr,
+                                    actual_location,
+                                    idx+1, ctr, index_list)
+            # print "ctr = ", ctr
+            # ctr+=1
+        index_list.pop(-1)
+        return idx-1, ctr
+
+
+# Run the code
 degree = 3
-q_hermite, w_hermite = np.polynomial.legendre.leggauss(degree) #np.polynomial.hermite.hermgauss(degree)
-univariate_normal = cp.Normal() # STandard normal
-nodes, weights = cp.generate_quadrature(2, univariate_normal, rule="G")
-print "q_hermite = ", q_hermite
-print "nodes = ", nodes
-print "w_hermite = ", w_hermite
-print "weights = ", weights
-
-
-
-
-
-
-
-"""
 systemsize = 2
-x = np.random.rand(systemsize) # np.zeros(systemsize)
-theta = 0
-sigma = np.ones(systemsize) # np.array([0.2, 0.1])
-tuple = (theta,)
+q,w = np.polynomial.hermite.hermgauss(degree)
+colloc_xi_arr = np.zeros(systemsize)
+colloc_w_arr = np.zeros(systemsize)
+std_dev = np.ones(systemsize)
+actual_location = np.zeros([degree**systemsize, systemsize])
+ctr = 0
+idx = 0
 
-# Create a Stochastic collocation object
-collocation = StochasticCollocation(3, "Normal")
-
-# Create a QoI object using ellpsoid
-QoI = examples.Paraboloid2D(systemsize, tuple)
-
-# Create a joint distribution
-jdist = cp.MvNormal(x, np.diag(sigma))
-"""
+idx, ctr = recursive_function(std_dev, q, w,colloc_xi_arr, colloc_w_arr,
+                              actual_location, idx, ctr)
