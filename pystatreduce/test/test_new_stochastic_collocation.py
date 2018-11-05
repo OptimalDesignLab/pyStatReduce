39869e6279e1c7af4a56f12f7e1a5f28c13b2010
Kinshuk Panda <pandak@rpi.edu>
2018-11-05 08:52:45 -0500

    added a new file that will contain tests for the new stochastic collocation library

---
+++ pystatreduce/test/test_new_stochastic_collocation.py
@@ -0,0 +1,17 @@
+# Test new_stochastic_collocation.py
+# Test stochastic collocation module
+import unittest
+import numpy as np
+import chaospy as cp
+
+from pystatreduce.stochastic_collocation import StochasticCollocation
+from pystatreduce.quantity_of_interest import QuantityOfInterest
+from pystatreduce.dimension_reduction import DimensionReduction
+import pystatreduce.examples as examples
+
+class NewStochasticCollocationTest(unittest.TestCase):
+	def test_normalStochasticCollocation3D(self):
+
+
+if __name__ == "__main__":
+    unittest.main()
