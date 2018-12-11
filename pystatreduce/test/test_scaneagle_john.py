f5d5a06dcff5174c58ad96610d8eb1f2311052fc
Kinshuk Panda <pandak@rpi.edu>
2018-12-11 15:51:00 -0500

    numba implementation delayed till travis can be enables, commit before debugging travis

--- pystatreduce/test/test_scaneagle_john.py
+++ pystatreduce/test/test_scaneagle_john.py
@@ -170,6 +170,7 @@ class OASScanEagleTest(unittest.TestCase):
         # library for the eigenvalue factorization. Which means that the Hessenberg
         # matrix which was factorized is in effect being tested with this.
         err = abs(dominant_space.iso_eigenvals - true_iso_eigenvals)
+        print('err = ', err)
         self.assertTrue((err < 1.e-6).all())
 
 if __name__ == "__main__":
